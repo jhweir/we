2c424a88b6a23e9a4582873c8cb936d28f04e0fe
jhweir <james@weco.io>
2025-04-26 22:49:28 +0100

    Next app moved to old directory, new vite app added with tsconfig setup

---
+++ apps/we/eslint.config.js
@@ -0,0 +1,25 @@
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import rootConfig from '../../eslint.config.js';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [...rootConfig],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+);
