b6c2f0d3897822614913fdf11a8e0d5c9bcdafb1
jhweir <james@weco.io>
2025-04-30 23:04:00 +0100

    React router set up

--- apps/we/src/templates/Default/components/OuterSidebars/SidebarLeft/index.tsx
+++ apps/we/src/templates/Default/components/OuterSidebars/SidebarLeft/index.tsx
@@ -1,7 +1,10 @@
 import { useAdamContext } from '@/contexts/AdamContext';
+import { useNavigate } from 'react-router-dom';
 import styles from '../index.module.scss';
 
-function CircleButton(props: { icon?: string; image?: string; initials?: string; onClick: () => void }) {
+const WECO_LOGO = 'https://avatars.githubusercontent.com/u/34165012?s=200&v=4';
+
+function CircleButton(props: { key?: string; icon?: string; image?: string; initials?: string; onClick: () => void }) {
   const { icon, image, initials, onClick } = props;
   return (
     <we-button circle variant="ghost" onClick={onClick}>
@@ -14,22 +17,22 @@ function CircleButton(props: { icon?: string; image?: string; initials?: string;
 
 export default function SidebarLeft() {
   const { mySpaces, setActiveModals } = useAdamContext();
-  const wecoLogo = 'https://avatars.githubusercontent.com/u/34165012?s=200&v=4';
+  const navigate = useNavigate();
 
   return (
     <we-column py="700" ax="center" ay="between" bg="ui-0" class={`${styles.sidebar} ${styles.left}`}>
       <we-column gap="400">
-        <CircleButton image={wecoLogo} onClick={() => router.push('/')} />
-        <CircleButton icon="magnifying-glass" onClick={() => router.push('/search')} />
-        <CircleButton icon="users-three" onClick={() => router.push('/all-spaces')} />
+        <CircleButton image={WECO_LOGO} onClick={() => navigate('/')} />
+        <CircleButton icon="magnifying-glass" onClick={() => navigate('/search')} />
+        <CircleButton icon="users-three" onClick={() => navigate('/all-spaces')} />
         {mySpaces.map((space) => (
-          <CircleButton key={space.uuid} initials={space.name} onClick={() => router.push(`/space/${space.uuid}`)} />
+          <CircleButton key={space.uuid} initials={space.name} onClick={() => navigate(`/space/${space.uuid}`)} />
         ))}
         <CircleButton icon="plus" onClick={() => setActiveModals((prev) => ({ ...prev, createSpace: true }))} />
       </we-column>
 
       <we-column gap="400">
-        <CircleButton icon="gear" onClick={() => router.push('/settings')} />
+        <CircleButton icon="gear" onClick={() => navigate('/settings')} />
       </we-column>
     </we-column>
   );
