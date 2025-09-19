63ff5eec773eba169646831f08a707400d491f02
jhweir <james@weco.io>
2025-09-19 16:35:38 +0200

    SidebarLeft updated to use new CircleButton component

--- apps/we/src/templates/Default/components/OuterSidebars/SidebarLeft/SidebarLeft.tsx
+++ apps/we/src/templates/Default/components/OuterSidebars/SidebarLeft/SidebarLeft.tsx
@@ -1,22 +1,11 @@
 import { useNavigate } from '@solidjs/router';
-import { Column } from '@we/components/solid';
+import { CircleButton, Column } from '@we/components/solid';
 
 import { useAdamStore, useModalStore } from '@/stores';
 
 import styles from '../OuterSidebars.module.scss';
 
 const WECO_LOGO = 'https://avatars.githubusercontent.com/u/34165012?s=200&v=4';
-
-function CircleButton(props: { key?: string; icon?: string; image?: string; initials?: string; onClick: () => void }) {
-  const { icon, image, initials, onClick } = props;
-  return (
-    <we-button circle variant="ghost" onClick={onClick}>
-      {icon && <we-icon name={icon} color="ui-700" />}
-      {image && <we-avatar image={image} />}
-      {initials && <we-avatar initials={initials} />}
-    </we-button>
-  );
-}
 
 export default function SidebarLeft() {
   const adamStore = useAdamStore();
@@ -26,17 +15,17 @@ export default function SidebarLeft() {
   return (
     <Column py="700" ax="center" ay="between" bg="ui-0" class={`${styles.sidebar} ${styles.left}`}>
       <Column gap="400">
-        <CircleButton image={WECO_LOGO} onClick={() => navigate('/')} />
-        <CircleButton icon="magnifying-glass" onClick={() => navigate('/search')} />
-        <CircleButton icon="users-three" onClick={() => navigate('/all-spaces')} />
+        <CircleButton name="Home" image={WECO_LOGO} onClick={() => navigate('/')} />
+        <CircleButton name="Search" icon="magnifying-glass" onClick={() => navigate('/search')} />
+        <CircleButton name="Spaces" icon="users-three" onClick={() => navigate('/all-spaces')} />
         {adamStore.state.mySpaces.map((space) => (
-          <CircleButton key={space.uuid} initials={space.name} onClick={() => navigate(`/space/${space.uuid}`)} />
+          <CircleButton name={space.name} onClick={() => navigate(`/space/${space.uuid}`)} />
         ))}
-        <CircleButton icon="plus" onClick={() => modalStore.actions.openModal('createSpace')} />
+        <CircleButton name="New space" icon="plus" onClick={() => modalStore.actions.openModal('createSpace')} />
       </Column>
 
       <Column gap="400">
-        <CircleButton icon="gear" onClick={() => navigate('/settings')} />
+        <CircleButton name="Settings" icon="gear" onClick={() => navigate('/settings')} />
       </Column>
     </Column>
   );
